01da366b26670ccfbc8bcc861739ffed8d673091
Brayan Cholotio <bcholotiot@miumg.edu.gt>
2026-07-20 01:48:45 -0600

    Configuración inicial Spring Boot y PostgresSQL

---
+++ backend/hospital-his-backend/src/main/java/com/hospital/his/HospitalHisBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hospital.his;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalHisBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalHisBackendApplication.class, args);
+    }
+
+}
---
+++ backend/hospital-his-backend/src/test/java/com/hospital/his/HospitalHisBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital.his;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalHisBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
